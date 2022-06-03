cd9dc71daad757df22075869b80572af33e31a63
onesmus <onesmus1024@gmail.com>
2022-06-03 18:47:13 +0300

    jenkin test

--- src/main/java/com/mycompany/cuopon/App.java
+++ src/main/java/com/mycompany/cuopon/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Jenkin webhook initial " );
+        System.out.println( "Jenkin webhook  " );
     }
 }
