ac679434d8d7ef74a82b605aba51e5f53f091027
onesmus <onesmus1024@gmail.com>
2022-06-03 18:18:32 +0300

    message update

--- src/main/java/com/mycompany/cuopon/App.java
+++ src/main/java/com/mycompany/cuopon/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Jenkin webhook initial test" );
+        System.out.println( "Jenkin webhook initial test test" );
     }
 }
